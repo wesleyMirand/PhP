0c2c589ae17da91b132e50734ee97876908c9270
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-08 23:59:09 -0300

    Create Exercito.php

---
+++ Exercito.php
@@ -0,0 +1,34 @@
+<?php
+
+$idade = 20;
+
+if ($idade >= 18) {
+  echo "maior de idade";
+}else { 
+  echo"menor de idade"; 
+}
+
+print '<br>';
+
+if ($idade >= 60) {
+  print "é um idoso";
+} else if ( $idade >= 18 ) {
+  print "é um adulto";
+}
+else if ($idade >= 12) {
+  print "é um adolescente";
+} else {
+  print "é uma criança";
+}
+
+print '<br>';
+
+$sexo = 'M';
+
+if ($idade >= 18 and $sexo =='M') {
+  echo "serviço obrigatorio";
+} else {
+  echo "nao tem ";
+}
+
+?>
