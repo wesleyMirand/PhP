f67fddf1a1b984f9945ec7189ccdc82493393c92
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-10-03 17:50:08 -0300

    Create Questãoprova11.php

---
+++ Questãoprova11.php
@@ -0,0 +1,16 @@
+<?php
+
+$multiplicador= 1;
+$x = 1;
+$resultado = null;
+
+while ( $x <= 20 ) {
+      #code...
+      $resultado = $x * $multiplicador;
+      echo "O resultado é $x x $multiplicador = $resultado<br>";
+      
+      $x = $x + 1;
+      
+}
+
+?>
