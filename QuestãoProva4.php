827c28a58f8f3bae1f5b007e148d4be32ef2af98
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-10-02 23:52:12 -0300

    Create QuestãoProva4.php

---
+++ QuestãoProva4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+     <title> Desafio </title>
+     <meta charset= "UTF-8" >
+</head>
+<form action = "desafio.php" name= "nn" method = "post" >
+    Nome: <input type="text" name="Nome"><br><br>
+    Idade:<input type="text" name="Idade"><br><br>
+    Sexo:<select name= "sexo">
+    
+<body>
+
+<?php
+$txt = "PHP";
+echo "I love $txt!";
+?>
+
+</body>
+</html>
