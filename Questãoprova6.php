517ffc12eb0d1a61379d7dd17e881be3640b0c18
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-10-03 17:46:55 -0300

    Create Questãoprova6.php

---
+++ Questãoprova6.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>Desafio</title>
+     <meta charset="UTF-8">
+</head>
+<form action="desafio.php" name="nm" method="post">
+      Nome:<input type="text" name="Nome"><br><br>
+      Idade:<input type="text" name="Idade"><br><br>
+      Sexo:<select name="sexo">
+           <option value="">Masculino</option><br>
+           <option value="">Feminino</option>
+      </select><br><br>
+      
+      <table>
+             Esportes: <br>
+             <input type="checkbox" name="Atividade" value="1">Futebol1<br>
+             <input type="checkbox" name="Atividade" value="2">Basquete<br>
+             <input type="checkbox" name="Atividade" value="3">Volei<br>
+             <input type="checkbox" name="Atividade" value="4">Handball<br>
+             <input type="checkbox" name="Atividade" value="5">Natação<br>
+             
+      </table>
+</form>
+
+<body>
+
+</body>
+</html>
+      
