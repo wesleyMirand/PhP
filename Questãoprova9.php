a3dd4ce5abb549abcdd9ffe77b81da05f37f8ed8
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-10-03 17:49:12 -0300

    Create Questãoprova9.php

---
+++ Questãoprova9.php
@@ -0,0 +1,18 @@
+<?php
+
+$mes = 3;
+
+switch ($mes) {
+     case 1:
+          print ("Janeiro");
+          break;
+     case 2:
+          print ("Fevereiro");
+          break;
+     case 3: 
+          print ("Março");
+          break;
+     default:
+          print("Mês não previsto no programa");
+}
+?>
