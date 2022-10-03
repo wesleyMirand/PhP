3e6915f3a25d1bb199461e715a4ec854baa0c730
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-10-02 23:51:47 -0300

    Create QuestãoProva3.php

---
+++ QuestãoProva3.php
@@ -0,0 +1,18 @@
+html>
+     <body>
+          <h1>Comparação de 2 números</h1>
+          <form action= "aula09.php" method="POST">
+             <p>
+                Digite o Número 1:
+                <input type= "text" name= "nro1">
+             </p>
+             <p>
+                Digite o Número 2:
+                <input type= "text" name="nro2">
+             </p>
+             <p>
+                <input type="submit" value="Enviar">
+             </p>
+          </form>
+     </body>
+</html>
